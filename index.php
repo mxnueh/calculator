1375b480c6d13c7232780aaf299d402a9731288f
mxnueh <enmanuelnunezmatias@gmail.com>
2025-05-03 21:58:19 -0400

    Uploading calculator files

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora | Enmanuel</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    <form action="" method="POST" >
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>10</button>
+        <button type="submit" class="item-equal">=</button>
+    </form>
+</body>
+</html>
